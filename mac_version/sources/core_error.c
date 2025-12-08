90f150517012e5a2d3737fca31d544d5c17e0018
Karintorn Suebthawilkul <karinsueb@gmail.com>
2025-12-08 18:05:24 +0100

    change

--- mac_version/sources/core_error.c
+++ mac_version/sources/core_error.c
@@ -3,7 +3,7 @@
 void ft_err(char *str, t_collector **g_root)
 {
     ft_printf("Error: %s\n", str);
-    if (g_root != NULL)
+    if (g_root && *g_root)
         gc_free_all(g_root);
     exit(1);
 }
--- mac_version/sources/main.c
+++ mac_version/sources/main.c
@@ -1,39 +1,65 @@
 #include "../include/minirt.h"
 
-// void parse_identifier(char* id, char**tokens, t_scene *sc)
-// {
-//     if (ft_strcmp(id, "A") == 0)
-//         parse_ambient();
-//     else if (ft_strcmp(id, "C") == 0)
-//         parse_camera();
-//     else if (ft_strcmp(id, "L") == 0)
-//         parse_light();
-//     else if (ft_strcmp(id, "sp") == 0)
-//         parse_sphere();
-//     else if (ft_strcmp(id, "pl") == 0)
-//         parse_plane();
-//     else if (ft_strcmp(id, "cy") == 0)
-//         parse_cylinder();
-//     else
-//         ft_err("Invaid Object Type", );
+void free_split(char **tokens)
+{
+    int i;
+
+    i = 0;
+    if (!tokens)
+        return;
+    while (tokens[i])
+    {
+        free(tokens[i]);
+        i++;
+    }
+    free(tokens);
+}
+
+void parse_identifier(char* id, char**tokens, t_data *data)
+{
+    if (ft_strcmp(id, "A") == 0)
+        parse_ambient();
+    else if (ft_strcmp(id, "C") == 0)
+        parse_camera();
+    else if (ft_strcmp(id, "L") == 0)
+        parse_light();
+    else if (ft_strcmp(id, "sp") == 0)
+        parse_sphere();
+    else if (ft_strcmp(id, "pl") == 0)
+        parse_plane();
+    else if (ft_strcmp(id, "cy") == 0)
+        parse_cylinder();
+    else
+    {
+        free_split(tokens);
+        ft_err("Invaid Object Type", &data->gc_root);
+    }
     
-// }
+}
 
-// int parse(t_scene *sc, int fd, t_collector *g_root)
-// {
-//     char **tokens;
 
-//     while(1)
-//     {
-//         tokens = ft_split(get_next_line(fd), ' ');
-//         if (!tokens)
-//             break;
-//         if (*tokens)
-//             parse_identifier(*tokens, tokens, sc);
-        
-//     }
-//     close(fd);
-// }
+
+int parse(t_data *data)
+{
+    char *line;
+    char **tokens;
+
+    while(1)
+    {
+        line = get_next_line(data->fd);
+        if (!line)
+            break;
+        tokens = ft_split(line, ' ');
+        free(line);
+        if (!tokens)
+            ft_err("malloc failed in ft_split", &data->gc_root);
+        if (*tokens)
+            parse_identifier(*tokens, tokens, data);
+        free_split(tokens);
+    }
+    close(data->fd);
+    return (0);
+}
 
 
 int main(int argc, char **argv)
@@ -46,7 +72,7 @@ int main(int argc, char **argv)
         ft_err("Incorrect argument: ./miniRT file_name.rt", &data.gc_root);
     if (!allocate_scene(&data))
         ft_err("allocation", &data.gc_root);
-    // parse(&data);
+    parse(&data);
     // .......program logic.......
     gc_free_all(&data.gc_root);
     // close(fd)

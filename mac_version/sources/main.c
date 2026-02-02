3278f26a23c37b0718a82b08bbea80c4845420c8
Karintorn Suebthawilkul <karinsueb@gmail.com>
2026-02-02 13:26:57 +0100

    delete unused function

--- mac_version/sources/main.c
+++ mac_version/sources/main.c
@@ -1,16 +1,5 @@
 #include "../include/minirt.h"
 
-
-// void my_mlx_pixel_put(t_image *img, int x, int y, int color)
-// {
-//     char *dst;
-//     if (x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT)
-//         return;
-//     dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel/8));
-//     *(unsigned int *)dst = (unsigned int)color;
-// }
-
-
 int main(int argc, char **argv)
 {
     t_data data;
@@ -23,9 +12,6 @@ int main(int argc, char **argv)
     parse(&data);
     print_scene(&data);
     rendering(&data);
-
-    // .......program logic.......
-    
     gc_free_all(&data.gc_root);
     // close(fd)
     return 0;

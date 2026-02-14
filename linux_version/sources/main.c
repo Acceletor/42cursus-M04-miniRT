29c7914aedd2d94e0bbe59aabcafeaa1ef7e7bf4
Debian <monandszy@tuta.io>
2026-02-14 20:07:16 +0100

    fix: commented out print_scene

--- linux_version/sources/main.c
+++ linux_version/sources/main.c
@@ -6,12 +6,13 @@
 /*   By: sandrzej&ksuebtha <student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/14 18:03:33 by sandrzej&ks       #+#    #+#             */
-/*   Updated: 2026/02/14 18:20:58 by sandrzej&ks      ###   ########.fr       */
+/*   Updated: 2026/02/14 20:06:33 by sandrzej&ks      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minirt.h"
 
+// print_scene(&data);
 int	main(int argc, char **argv)
 {
 	t_data	data;
@@ -22,7 +23,6 @@ int	main(int argc, char **argv)
 		ft_err("Incorrect argument: ./miniRT file_name.rt", &data.gc_root, 1);
 	init_scene(&data);
 	parse(&data);
-	print_scene(&data);
 	rendering(&data);
 	gc_free_all(&data.gc_root);
 	return (0);
--- linux_version/sources/utils/print_scene.c
+++ linux_version/sources/utils/print_scene.c
@@ -6,130 +6,130 @@
 /*   By: sandrzej&ksuebtha <student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/14 18:03:28 by sandrzej&ks       #+#    #+#             */
-/*   Updated: 2026/02/14 18:03:29 by sandrzej&ks      ###   ########.fr       */
+/*   Updated: 2026/02/14 20:04:32 by sandrzej&ks      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minirt.h"
 
-void	print_vec(char *label, t_vec v)
-{
-	printf("%s(%.2f, %.2f, %.2f)\n", label, v.x, v.y, v.z);
-}
+// void	print_vec(char *label, t_vec v)
+// {
+// 	printf("%s(%.2f, %.2f, %.2f)\n", label, v.x, v.y, v.z);
+// }
 
-void	print_ambient(t_amb *amb)
-{
-	if (!amb->is_set)
-	{
-		printf("Ambient: not set\n");
-		return ;
-	}
-	printf("=== Ambient ===\n");
-	printf("ratio: %.3f\n", amb->ratio);
-	print_vec("color: ", amb->color);
-}
+// void	print_ambient(t_amb *amb)
+// {
+// 	if (!amb->is_set)
+// 	{
+// 		printf("Ambient: not set\n");
+// 		return ;
+// 	}
+// 	printf("=== Ambient ===\n");
+// 	printf("ratio: %.3f\n", amb->ratio);
+// 	print_vec("color: ", amb->color);
+// }
 
-void	print_camera(t_cam *cam)
-{
-	if (!cam->is_set)
-	{
-		printf("Camera: not set\n");
-		return ;
-	}
-	printf("=== Camera ===\n");
-	print_vec("pos:   ", cam->pos);
-	print_vec("dir:   ", cam->dir);
-	printf("fov: %.2f\n", cam->fov);
-}
+// void	print_camera(t_cam *cam)
+// {
+// 	if (!cam->is_set)
+// 	{
+// 		printf("Camera: not set\n");
+// 		return ;
+// 	}
+// 	printf("=== Camera ===\n");
+// 	print_vec("pos:   ", cam->pos);
+// 	print_vec("dir:   ", cam->dir);
+// 	printf("fov: %.2f\n", cam->fov);
+// }
 
-void	print_lights_and_objs(t_scene *sc)
-{
-	t_light	*li;
-	t_objs	*ob;
-	int		i;
+// void	print_lights_and_objs(t_scene *sc)
+// {
+// 	t_light	*li;
+// 	t_objs	*ob;
+// 	int		i;
 
-	li = sc->light;
-	i = 0;
-	printf("=== Lights ===\n");
-	if (!li)
-		printf("none\n");
-	while (li)
-	{
-		printf("Light #%d\n", i++);
-		print_vec("  pos:   ", li->pos);
-		printf("  ratio: %.3f\n", li->ratio);
-		print_vec("  color: ", li->color);
-		li = li->next;
-	}
-	printf("\n");
-	ob = sc->objs;
-	i = 0;
-	printf("=== Objects ===\n");
-	if (!ob)
-		printf("none\n");
-	while (ob)
-	{
-		printf("Object #%d: ", i++);
-		if (ob->type == OBJ_SPHERE)
-		{
-			printf("Sphere\n");
-			print_vec("  center:  ", ((t_sphere *)ob->data)->center);
-			printf("  diam:    %.3f\n", ((t_sphere *)ob->data)->diameter);
-			print_vec("  color:   ", ((t_sphere *)ob->data)->color);
-		}
-		else if (ob->type == OBJ_PLANE)
-		{
-			printf("Plane\n");
-			print_vec("  point:  ", ((t_plane *)ob->data)->point);
-			print_vec("  normal:  ", ((t_plane *)ob->data)->normal);
-			print_vec("  color:   ", ((t_plane *)ob->data)->color);
-		}
-		else if (ob->type == OBJ_CYLINDER)
-		{
-			printf("Cylinder\n");
-			print_vec("  center:  ", ((t_cylinder *)ob->data)->center);
-			print_vec("  normal:  ", ((t_cylinder *)ob->data)->normal);
-			printf("  diam:    %.3f\n", ((t_cylinder *)ob->data)->diameter);
-			printf("  height:    %.3f\n", ((t_cylinder *)ob->data)->height);
-			print_vec("  color:  ", ((t_cylinder *)ob->data)->color);
-		}
-		else if (ob->type == OBJ_CONE)
-		{
-			printf("Cone\n");
-			print_vec("  center:  ", ((t_cylinder *)ob->data)->center);
-			print_vec("  normal:  ", ((t_cylinder *)ob->data)->normal);
-			printf("  diam:    %.3f\n", ((t_cylinder *)ob->data)->diameter);
-			printf("  height:    %.3f\n", ((t_cylinder *)ob->data)->height);
-			print_vec("  color:  ", ((t_cylinder *)ob->data)->color);
-		}
-		else if (ob->type == OBJ_TUBE)
-		{
-			printf("Tube\n");
-			print_vec("  center:  ", ((t_cylinder *)ob->data)->center);
-			print_vec("  normal:  ", ((t_cylinder *)ob->data)->normal);
-			printf("  diam:    %.3f\n", ((t_cylinder *)ob->data)->diameter);
-			printf("  height:    %.3f\n", ((t_cylinder *)ob->data)->height);
-			print_vec("  color:  ", ((t_cylinder *)ob->data)->color);
-		}
-		else if (ob->type == OBJ_CIRCLE)
-		{
-			printf("Circle\n");
-			print_vec("  center:  ", ((t_circle *)ob->data)->center);
-			print_vec("  normal:  ", ((t_circle *)ob->data)->normal);
-			printf("  diam:    %.3f\n", ((t_circle *)ob->data)->diameter);
-			print_vec("  color:  ", ((t_circle *)ob->data)->color);
-		}
-		ob = ob->next;
-	}
-}
+// 	li = sc->light;
+// 	i = 0;
+// 	printf("=== Lights ===\n");
+// 	if (!li)
+// 		printf("none\n");
+// 	while (li)
+// 	{
+// 		printf("Light #%d\n", i++);
+// 		print_vec("  pos:   ", li->pos);
+// 		printf("  ratio: %.3f\n", li->ratio);
+// 		print_vec("  color: ", li->color);
+// 		li = li->next;
+// 	}
+// 	printf("\n");
+// 	ob = sc->objs;
+// 	i = 0;
+// 	printf("=== Objects ===\n");
+// 	if (!ob)
+// 		printf("none\n");
+// 	while (ob)
+// 	{
+// 		printf("Object #%d: ", i++);
+// 		if (ob->type == OBJ_SPHERE)
+// 		{
+// 			printf("Sphere\n");
+// 			print_vec("  center:  ", ((t_sphere *)ob->data)->center);
+// 			printf("  diam:    %.3f\n", ((t_sphere *)ob->data)->diameter);
+// 			print_vec("  color:   ", ((t_sphere *)ob->data)->color);
+// 		}
+// 		else if (ob->type == OBJ_PLANE)
+// 		{
+// 			printf("Plane\n");
+// 			print_vec("  point:  ", ((t_plane *)ob->data)->point);
+// 			print_vec("  normal:  ", ((t_plane *)ob->data)->normal);
+// 			print_vec("  color:   ", ((t_plane *)ob->data)->color);
+// 		}
+// 		else if (ob->type == OBJ_CYLINDER)
+// 		{
+// 			printf("Cylinder\n");
+// 			print_vec("  center:  ", ((t_cylinder *)ob->data)->center);
+// 			print_vec("  normal:  ", ((t_cylinder *)ob->data)->normal);
+// 			printf("  diam:    %.3f\n", ((t_cylinder *)ob->data)->diameter);
+// 			printf("  height:    %.3f\n", ((t_cylinder *)ob->data)->height);
+// 			print_vec("  color:  ", ((t_cylinder *)ob->data)->color);
+// 		}
+// 		else if (ob->type == OBJ_CONE)
+// 		{
+// 			printf("Cone\n");
+// 			print_vec("  center:  ", ((t_cylinder *)ob->data)->center);
+// 			print_vec("  normal:  ", ((t_cylinder *)ob->data)->normal);
+// 			printf("  diam:    %.3f\n", ((t_cylinder *)ob->data)->diameter);
+// 			printf("  height:    %.3f\n", ((t_cylinder *)ob->data)->height);
+// 			print_vec("  color:  ", ((t_cylinder *)ob->data)->color);
+// 		}
+// 		else if (ob->type == OBJ_TUBE)
+// 		{
+// 			printf("Tube\n");
+// 			print_vec("  center:  ", ((t_cylinder *)ob->data)->center);
+// 			print_vec("  normal:  ", ((t_cylinder *)ob->data)->normal);
+// 			printf("  diam:    %.3f\n", ((t_cylinder *)ob->data)->diameter);
+// 			printf("  height:    %.3f\n", ((t_cylinder *)ob->data)->height);
+// 			print_vec("  color:  ", ((t_cylinder *)ob->data)->color);
+// 		}
+// 		else if (ob->type == OBJ_CIRCLE)
+// 		{
+// 			printf("Circle\n");
+// 			print_vec("  center:  ", ((t_circle *)ob->data)->center);
+// 			print_vec("  normal:  ", ((t_circle *)ob->data)->normal);
+// 			printf("  diam:    %.3f\n", ((t_circle *)ob->data)->diameter);
+// 			print_vec("  color:  ", ((t_circle *)ob->data)->color);
+// 		}
+// 		ob = ob->next;
+// 	}
+// }
 
-void	print_scene(t_data *data)
-{
-	printf("===== SCENE DUMP =====\n");
-	print_ambient(&data->scene.amb);
-	printf("\n");
-	print_camera(&data->scene.cam);
-	printf("\n");
-	print_lights_and_objs(&data->scene);
-	printf("\n");
-}
+// void	print_scene(t_data *data)
+// {
+// 	printf("===== SCENE DUMP =====\n");
+// 	print_ambient(&data->scene.amb);
+// 	printf("\n");
+// 	print_camera(&data->scene.cam);
+// 	printf("\n");
+// 	print_lights_and_objs(&data->scene);
+// 	printf("\n");
+// }
